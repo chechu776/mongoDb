aab40007c922427d0adf389bb5f1decf8d737495
chechu776 <shameerchechu@gmail.com>
2025-06-26 17:41:26 +0530

    4

---
+++ 4.js
@@ -0,0 +1,32 @@
+db.orders.aggregate([
+    {
+        $group: {
+            _id: {
+                year: { $year: "$order_date" },
+                month: { $month: "$order_date" },
+
+            },
+
+            total_amount: { $sum: "$total_amount" },
+        }
+    },
+    {
+        $sort:{"_id.year":1,total_amount:-1}
+    },
+    {
+        $group:{
+            _id:"$_id.year",
+            month:{$first:"$_id.month"},
+            total_amount:{$first:"$total_amount"}
+        }
+    },
+    // {
+    //     $group:{
+    //         _id:"$_id.year",
+    //         maxTotalAmount:{$max:"$total_amount"}
+    //     }
+    // },
+    {
+        $sort: ({ "_id": 1 })
+    },
+])
